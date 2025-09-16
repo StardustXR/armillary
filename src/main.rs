4cd9d6461a4fa4ab078cbdc2600acab6626fbbd0
Schmarni <marnistromer@gmail.com>
2025-09-16 05:23:21 +0200

    chore: use the new asteroids name
    Signed-off-by: Schmarni <marnistromer@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use asteroids::{
-    client::ClientState,
-    elements::{Bounds, FileWatcher, GrabRing, Model, Text, Turntable},
-    ElementTrait, Migrate, Transformable,
-};
 use clap::Parser;
 use mint::Vector3;
 use serde::{Deserialize, Serialize};
+use stardust_xr_asteroids::{
+    client::ClientState,
+    elements::{Bounds, FileWatcher, GrabRing, Model, Text, Turntable},
+    CustomElement as _, Identifiable, Migrate, Reify, Transformable,
+};
 use stardust_xr_fusion::drawable::XAlign;
 use std::{path::PathBuf, sync::OnceLock};
 use tracing_subscriber::EnvFilter;
@@ -48,15 +48,14 @@ impl Migrate for State {
     type Old = Self;
 }
 impl ClientState for State {
-    const QUALIFIER: &'static str = "org";
-    const ORGANIZATION: &'static str = "stardustxr";
-    const NAME: &'static str = "armillary";
-
+    const APP_ID: &'static str = "org.stardustxr.armillary";
     fn initial_state_update(&mut self) {
         let args = Args::parse();
         self.model_path = args.file_path.canonicalize().unwrap();
     }
-    fn reify(&self) -> asteroids::Element<Self> {
+}
+impl Reify for State {
+    fn reify(&self) -> impl stardust_xr_asteroids::Element<Self> {
         let model_info = self.model_info.get_or_init(|| {
             let uuid = Uuid::new_v4();
             println!("creating new model info with uuid {uuid}");
@@ -66,50 +65,63 @@ impl ClientState for State {
                 scale: 0.0,
             }
         });
-
-        let model = match Model::direct(&self.model_path) {
-            Ok(model) => model
-                .pos([0.0, model_info.height_offset, 0.0])
-                .build()
-                .identify(&model_info.uuid),
-            Err(e) => Text::default()
-                .text(format!("Model Error:\n{e}"))
-                .text_align_x(XAlign::Center)
-                .character_height(0.025)
-                .pos([0.0, 0.075, 0.0])
-                .build(),
+        let mut model = None;
+        let mut model_error = None;
+        match Model::direct(&self.model_path) {
+            Ok(model_elem) => {
+                model = Some(
+                    model_elem
+                        .pos([0.0, model_info.height_offset, 0.0])
+                        .build()
+                        .identify(&model_info.uuid),
+                )
+            }
+            Err(e) => {
+                model_error = Some(
+                    Text::new(format!("Model Error:\n{e}"))
+                        .align_x(XAlign::Center)
+                        .character_height(0.025)
+                        .pos([0.0, 0.075, 0.0])
+                        .build(),
+                )
+            }
         };
-
-        let bounds = Bounds::new(|state: &mut State, bounds| {
-            let Some(model_info) = state.model_info.get_mut() else {
-                return;
-            };
-
-            model_info.height_offset = bounds.size.y / 2.0;
-
-            let min_size = bounds.size.x.min(bounds.size.z);
-            model_info.scale = state.radius * 2.0 / min_size;
-        })
-        .scl([model_info.scale; 3])
-        .with_children([model]);
-
-        let file_watcher = FileWatcher::new(self.model_path.clone(), |state: &mut State| {
-            println!("file is modified");
-            state.model_info.take();
-        })
-        .build();
-        let turntable = Turntable::new(self.turntable_angle, |state: &mut State, angle| {
-            state.turntable_angle = angle;
-        })
-        .pos([0.0, 0.035, 0.0])
-        .inner_radius(self.radius)
-        .with_children([bounds, file_watcher]);
-
         GrabRing::new(self.pos, |state: &mut State, pos| {
             state.pos = pos;
         })
-        .radius(self.radius + 0.04)
-        .with_children([turntable])
+        .radius(self.radius)
+        .thickness(0.02)
+        .build()
+        .child(
+            Turntable::new(self.turntable_angle, |state: &mut State, angle| {
+                state.turntable_angle = angle;
+            })
+            .pos([0.0, 0.035, 0.0])
+            .build(),
+        )
+        .child(
+            Bounds::new(|state: &mut State, bounds| {
+                let Some(model_info) = state.model_info.get_mut() else {
+                    return;
+                };
+
+                model_info.height_offset = bounds.size.y / 2.0;
+
+                let min_size = bounds.size.x.min(bounds.size.z);
+                model_info.scale = state.radius * 2.0 / min_size;
+            })
+            .scl([model_info.scale; 3])
+            .build()
+            .maybe_child(model)
+            .maybe_child(model_error),
+        )
+        .child(
+            FileWatcher::new(self.model_path.clone(), |state: &mut State| {
+                println!("file is modified");
+                state.model_info.take();
+            })
+            .build(),
+        )
     }
 }
 
@@ -120,5 +132,5 @@ async fn main() {
         .with_env_filter(EnvFilter::from_env("LOG_LEVEL"))
         .init();
     // let args = Args::parse();
-    asteroids::client::run::<State>(&[]).await
+    stardust_xr_asteroids::client::run::<State>(&[]).await
 }
